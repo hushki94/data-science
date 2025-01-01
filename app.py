040b435dcca8c2e552aa3b37a74bacef952f9413
hushki94 <omarhushki94@gmail.com>
2025-01-01 01:56:45 -0800

    Add sidebar filters for fuel and condition.

--- app.py
+++ app.py
@@ -9,3 +9,11 @@ data = pd.read_csv(DATA_FILE)
 
 # Streamlit app title
 st.title("Interactive Vehicle Data Dashboard")
+
+# Sidebar filters
+st.sidebar.header("Filters")
+fuel_filter = st.sidebar.multiselect("Select Fuel Type", data['fuel'].unique(), default=data['fuel'].unique())
+condition_filter = st.sidebar.multiselect("Select Condition", data['condition'].unique(), default=data['condition'].unique())
+
+# Filter the data
+filtered_data = data[(data['fuel'].isin(fuel_filter)) & (data['condition'].isin(condition_filter))]
