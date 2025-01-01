c050f4fc8ff8bb5df5b8b3760422c5542b240324
hushki94 <omarhushki94@gmail.com>
2025-01-01 01:52:18 -0800

    Add base Streamlit setup with data loading with jupyter notebook.

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 import streamlit as st
+import pandas as pd
+import altair as alt
+import plotly.express as px
 
-st.header('Tossing a Coin')
+# Load the dataset
+DATA_FILE = "vehicles_us.csv"  # Replace with your dataset name
+data = pd.read_csv(DATA_FILE)
 
-st.write('It is not a functional application yet. Under construction.')
+# Streamlit app title
+st.title("Interactive Vehicle Data Dashboard")
